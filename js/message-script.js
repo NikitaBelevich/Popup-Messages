fe009808640c4427e803f9689297643876da73d7
Nikita Belevich <belevish_1998@mail.ru>
2021-02-05 18:15:45 +0300

    feat: Переделал плагин
    Теперь вся реализация сообщений находится в классе, и сообщение теперь инициализируется с помощью метода класса и становится объектом.

--- js/message-script.js
+++ js/message-script.js
@@ -3,38 +3,6 @@
 
 const addMessage = document.querySelector('#add-mes');
 addMessage.addEventListener('click', () => {
-    const createdMessage = createMessage('Hello World!');
-    addMessageWrapper(createdMessage);
-    deleteMessage(createdMessage);
+    const createdMessage = new PopupMessage('Hello world!');
+    createdMessage.initialization();
 });
-
-function createMessage(textMessage, backgroundMessage = '#e4aff5') {
-    const message = document.createElement('div');
-    message.classList.add('popup-massage');
-    message.style.background = backgroundMessage;
-    message.textContent = textMessage;
-    return message;
-}
-
-function addMessageWrapper(nodeMessage) {
-    let messageWrap = document.querySelector('.message-wrapper');
-    // Если нет контейнера для сообщений, то мы его создаём
-    if (!messageWrap) {
-        messageWrap = document.createElement('div');
-        messageWrap.classList.add('message-wrapper');
-        document.body.append(messageWrap);
-    }
-    // Добавили в контейнер сообщение, контейнер либо был, либо мы его создали
-    messageWrap.append(nodeMessage);
-}
-
-function deleteMessage(nodeMessage, delayRemove = 3000) {
-    setTimeout(() => {
-        nodeMessage.remove();
-        const allMessage = document.querySelectorAll('.popup-massage');
-        // Если сообщений больше нет, удаляем контейнер для них
-        if (allMessage.length == 0) {
-            document.querySelector('.message-wrapper').remove();
-        }
-    }, delayRemove);
-}
